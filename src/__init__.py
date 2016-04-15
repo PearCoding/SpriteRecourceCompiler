6e4bc107e30234125f48558efe935e798c929d16
pearcoding <pearcoding@gmail.com>
2016-04-16 00:50:19 +0200

    init.py fix

--- src/__init__.py
+++ src/__init__.py
@@ -1,22 +1,22 @@
-from exceptions import *
+from .exceptions import *
 
-from modes import *
-from tile import Tile
+from .modes import *
+from .tile import Tile
 
-from processor.processor import *
-from processor.parser import Parser
+from .processor.processor import *
+from .processor.parser import Parser
 
-from padding.padding import Padding
-from padding.color_padding import ColorPadding
-from padding.fill_padding import FillPadding
+from .padding.padding import Padding
+from .padding.color_padding import ColorPadding
+from .padding.fill_padding import FillPadding
 
-from filter.filter import Filter
-from filter.std_filter import StandardFilter
+from .filter.filter import Filter
+from .filter.std_filter import StandardFilter
 
-from packer.packer import Packer
-from packer.tight_packer import TightPacker
+from .packer.packer import Packer
+from .packer.tight_packer import TightPacker
 
-from csv_writer import CSVWriter
+from .csv_writer import CSVWriter
 
 VERSION = [0, 1]
 VERSION_STR = '{}.{}'.format(VERSION[0], VERSION[1])
